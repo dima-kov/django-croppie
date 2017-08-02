e843c051bae21d569c6d7897dd012e781905625c
Dima Kovalchuk <dmyutro@ukr.net>
2017-08-02 13:47:45 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+setup(
+    name='django-croppie',
+    version='0.1',
+    packages=find_packages(),
+    license='BSD License',
+    include_package_data=True,
+    description='Django application for image cropping during upload',
+    author='Dima Kovalchuk',
+    author_email='dmyutro@ukr.net',
+    url='',
+    download_url='',
+    keywords=['django', 'croppie', 'cropper', 'image-crop'],
+    install_requires=[],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
