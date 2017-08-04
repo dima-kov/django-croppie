6ac1a5b494bdebb7555d58b12b98d2cc2af047cb
Dima Kovalchuk <dmyutro@ukr.net>
2017-08-04 09:05:54 +0300

    Drop jquery

--- croppie/static/croppie/form-process.js
+++ croppie/static/croppie/form-process.js
@@ -1,11 +1,11 @@
-var cropper = $('#cropper').croppie(croppieOptions);
-
+var element = document.getElementById('cropper');
+var cropper = new Croppie(element, croppieOptions);
 
 function readFile(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function(e) {
-            $('#cropper').croppie('bind', {
+            cropper.bind({
                 url: e.target.result
             });
         }
@@ -13,14 +13,17 @@ function readFile(input) {
     }
 }
 
-$('input#id_photo_0').on('change', function() {
+var photoInput = document.getElementById('id_photo_0');
+photoInput.addEventListener('change', function() {
     readFile(this);
 });
 
-cropper.on('update', function(cr) {
-    var data = cropper.croppie('get');
-    var baseSelector = 'input#id_photo_';
+element.addEventListener('update', function(cr) {
+    var data = cropper.get();
+    var baseSelector = 'id_photo_';
+    var pointInput = null;
     for (var i = 1; i <= 4; i++) {
-        $(baseSelector + i).val(data.points[i-1]);
+        pointInput = document.getElementById(baseSelector + i)
+        pointInput.value = data.points[i-1];
     }
 });
