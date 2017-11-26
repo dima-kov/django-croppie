8158b47e5a1782dc97dd3c961d3afdba1745b58b
Dima Kovalchuk <dmyutro@ukr.net>
2017-11-26 15:07:25 +0200

    Event on cropper created

--- croppie/static/croppie/form-process.js
+++ croppie/static/croppie/form-process.js
@@ -1,5 +1,6 @@
 var element = document.getElementById('cropper');
 var cropper = new Croppie(element, croppieOptions);
+var cropperCreatedEvent = new Event('cropper-created');
 
 function readFile(input) {
     if (input.files && input.files[0]) {
@@ -8,6 +9,7 @@ function readFile(input) {
             cropper.bind({
                 url: e.target.result
             });
+            document.dispatchEvent(cropperCreatedEvent);
         }
         reader.readAsDataURL(input.files[0]);
     }
